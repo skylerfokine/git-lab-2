36a5a832dbb6b1cd96fc0cae4140329985f8557f
skylerfokine <skylerfokine@gmail.com>
2024-01-24 17:26:04 -0500

    Implemented code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Skyler Fokine
+ *      Date: 1/24/24
+ *      Description: Git-lab code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl; 
+    return 0;
+}// main
